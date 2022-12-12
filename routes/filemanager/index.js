0c7ec1150a63fa9e529cb9e3756b1303ab23e6e7
Mustafa <mustafaa.ylmz@icloud.com>
2022-12-12 23:33:12 +0300

    Added the ability to add files to folders

--- routes/filemanager/index.js
+++ routes/filemanager/index.js
@@ -1,52 +1,68 @@
-const File = require('../../models/File')
+const File = require("../../models/File");
 
-const multer = require('multer');
-const router = require('express').Router()
+const multer = require("multer");
+const router = require("express").Router();
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+const Directory = require("../../models/Directory");
 
-let files
-let fileSize
-const fileUID =  uuidv4()
+let files;
+let fileSize;
+const fileUID = uuidv4();
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-
-      cb(null, 'App_Data/'+req.session.useruId);
-      fileSize = parseInt(req.headers["content-length"])
-      files = file
+  destination: async(req, file, cb) => {
+    const dir = await Directory.findOne({where: {
+        id: req.params.id
+    }})
+    cb(null, "App_Data/" + req.session.useruId+'/'+dir.dir_name);
+    fileSize = parseInt(req.headers["content-length"]);
+    files = file;
   },
   filename: (req, file, cb) => {
-
     cb(null, fileUID);
-    
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
 
-router.get('/:id', async (req,res)=> {
-    const dirUId = req.params.id
-    const files = await File.findAll({where:{directoryId: dirUId}})
-    res.render('filemanager/index',{files})
-})
+router.get("/:id", async (req, res) => {
+  const dirUId = req.params.id;
+  const files = await File.findAll({ where: { directoryId: dirUId } });
+  res.render("filemanager/index", { files, dirUId });
+});
 
-
-router.post('/', upload.single('myFile'),async(req,res,next)=>{
+router.post("/:id", upload.single("myFile"), async (req, res, next) => {
+  const directoryId = req.params.id
   await File.create({
     file_id: fileUID,
     file_name: files.originalname,
     file_extension: files.mimetype,
-    file_size: fileSize
-  })
+    file_size: fileSize,
+    directoryId: directoryId,
+  });
   try {
     return res.status(201).json({
-        message: 'File uploded successfully'
-        
+      message: "File uploded successfully",
     });
-} catch (error) {
+  } catch (error) {
     console.error(error);
-}
+  }
+});
 
-})
-module.exports = router
+router.post("/", upload.single("myFile"), async (req, res, next) => {
+  await File.create({
+    file_id: fileUID,
+    file_name: files.originalname,
+    file_extension: files.mimetype,
+    file_size: fileSize,
+  });
+  try {
+    return res.status(201).json({
+      message: "File uploded successfully",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+module.exports = router;
--- tools/createVirtualDir.js
+++ tools/createVirtualDir.js
@@ -1,18 +1,16 @@
+var fs = require("fs");
+const Directory = require("../models/Directory");
 
-
-var fs = require('fs');
-const Directory = require('../models/Directory')
-
-
-const createDir=async(req)=> {
-var dir = './App_Data/'+req.session.useruId+'/'+req.body.dir_name+'/';
-if (!fs.existsSync(dir)){
+const createDir = async (req) => {
+  var dir = "./App_Data/" + req.session.useruId + "/" + req.body.dir_name + "/";
+  if (!fs.existsSync(dir)) {
     await fs.mkdirSync(dir);
     await Directory.create({
-        dir_name: req.body.dir_name,
-        dir_description: req.body.dir_description,
-        userId: req.session.userId
-    })
-}}
+      dir_name: req.body.dir_name,
+      dir_description: req.body.dir_description,
+      userId: req.session.userId,
+    });
+  }
+};
 
-module.exports = createDir
+module.exports = createDir;
